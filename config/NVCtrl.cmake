866d0d815967c2fd80259a824820c8320ee75154
Marwan Abdellah <abdellah.marwan@gmail.com>
2012-05-03 15:07:10 +0200

    Adding the NVCtrl.cmake

---
+++ config/NVCtrl.cmake
@@ -0,0 +1,11 @@
+if(MSVC OR APPLE)
+  return()
+endif()
+
+set(NVCTRL_VERSION 1.0.0)
+set(NVCTRL_REPO_URL git@github.com:marwan-abdellah/NVCtrl.git)
+set(NVCTRL_REPO_TAG master)
+set(NVCTRL_DEPENDS x11) 
+
+
+
